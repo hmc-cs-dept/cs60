efeadf0e04ada0984e2c874ac20e3872b48d35f7
breeden <kb@cs.hmc.edu>
2025-03-14 17:21:14 -0700

    Updating HW7 starter to adhere to class style guide

--- hw7/HochTable.java
+++ hw7/HochTable.java
@@ -7,126 +7,126 @@ import java.util.ArrayList;
  * (There's always room for N+1.)
  */
 public class HochTable {
-	/**
-	 * The default number of chairs.
-	 */
-	public static final int NUMBER_OF_CHAIRS = 6;
+    /**
+     * The default number of chairs.
+     */
+    public static final int NUMBER_OF_CHAIRS = 6;
 
-	private int numChairs;
-	private int numPeople;
-	private ArrayList<String> people;
+    private int numChairs;
+    private int numPeople;
+    private ArrayList<String> people;
 
-	/**
-	 * Constructs a table with the default number of chairs.
-	 */
-	public HochTable() {
-		this(HochTable.NUMBER_OF_CHAIRS);
-	}
+    /**
+     * Constructs a table with the default number of chairs.
+     */
+    public HochTable() {
+        this(HochTable.NUMBER_OF_CHAIRS);
+    }
 
-	/**
-	 * Constructs a table with the specified number of chairs.
-	 * 
-	 * @param capacity - the number of seats
-	 * @throws IllegalArgumentException if the capacity is less than 0
-	 */
-	public HochTable(int capacity) {
-		if (capacity < 0) {
-			throw new IllegalArgumentException("Tables must start with a capacity of 0 or more");
-		}
-		numChairs = capacity;
-		numPeople = 0;
-		people = new ArrayList<String>();
-	}
+    /**
+     * Constructs a table with the specified number of chairs.
+     * 
+     * @param capacity - the number of seats
+     * @throws IllegalArgumentException if the capacity is less than 0
+     */
+    public HochTable(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Tables must start with a capacity of 0 or more");
+        }
+        this.numChairs = capacity;
+        this.numPeople = 0;
+        this.people = new ArrayList<String>();
+    }
 
-	/**
-	 * Returns the number of chairs at this table.
-	 */
-	public int getNumChairs() {
-		return numChairs;
-	}
+    /**
+     * Returns the number of chairs at this table.
+     */
+    public int getNumChairs() {
+        return this.numChairs;
+    }
 
-	/**
-	 * Returns the number of people at this table.
-	 */
-	public int getNumPeople() {
-		return numPeople;
-	}
+    /**
+     * Returns the number of people at this table.
+     */
+    public int getNumPeople() {
+        return this.numPeople;
+    }
 
-	/**
-	 * Returns list of people at this table.
-	 */
-	public String getPeople() {
-		return people.toString();
-	}
+    /**
+     * Returns list of people at this table.
+     */
+    public String getPeople() {
+        return this.people.toString();
+    }
 
-	/**
-	 * Returns true because Hoch tables are never full.
-	 */
-	public boolean emptySeat() {
-		return true;
-	}
+    /**
+     * Returns true because Hoch tables are never full.
+     */
+    public boolean emptySeat() {
+        return true;
+    }
 
-	/**
-	 * Adds a person to this table if there is space.
-	 * 
-	 * @param name the name of the person to add
-	 * @return a welcome message
-	 */
-	public String addPerson(String name) {
-		// add a chair if needed
-		if (numChairs == numPeople) {
-			numChairs++;
-		}
+    /**
+     * Adds a person to this table if there is space.
+     * 
+     * @param name the name of the person to add
+     * @return a welcome message
+     */
+    public String addPerson(String name) {
+        // add a chair if needed
+        if (this.numChairs == this.numPeople) {
+            this.numChairs++;
+        }
 
-		// greet the person
-		if (emptySeat()) {
-			numPeople++;
-			people.add(name);
-			if (numPeople == 1) {
-				return "(Silence - no one is here to say hello.)";
-			}
-			return "Welcome " + name + "!";
-		} else {
-			if (numPeople == 0) {
-				return "(Silence - no one is here to say sorry.)";
-			}
-			return "Sorry - there is no space for you " + name + ".";
-		}
-	}
+        // greet the person
+        if (this.emptySeat()) {
+            this.numPeople++;
+            this.people.add(name);
+            if (this.numPeople == 1) {
+                return "(Silence - no one is here to say hello.)";
+            }
+            return "Welcome " + name + "!";
+        } else {
+            if (this.numPeople == 0) {
+                return "(Silence - no one is here to say sorry.)";
+            }
+            return "Sorry - there is no space for you " + name + ".";
+        }
+    }
 
-	/**
-	 * Removes a person from this table.
-	 * 
-	 * @param name - the name of the person to remove
-	 * @return a goodbye message
-	 */
-	public String removePerson(String name) {
-		boolean wasRemoved = people.remove(name);
-		if (!wasRemoved) {
-			return "Weird! " + name + " was not here!";
-		}
+    /**
+     * Removes a person from this table.
+     * 
+     * @param name - the name of the person to remove
+     * @return a goodbye message
+     */
+    public String removePerson(String name) {
+        boolean wasRemoved = this.people.remove(name);
+        if (!wasRemoved) {
+            return "Weird! " + name + " was not here!";
+        }
 
-		numPeople--;
-		if (numPeople == 0) {
-			return "(Silence - no one is here to say goodbye.)";
-		}
-		return "Bye " + name + ".";
-	}
+        this.numPeople--;
+        if (this.numPeople == 0) {
+            return "(Silence - no one is here to say goodbye.)";
+        }
+        return "Bye " + name + ".";
+    }
 
-	/**
-	 * Clears the table as everyone goes to class.
-	 * 
-	 * @return a goodbye message
-	 */
-	public String classStartingSoon() {
-		String message;
-		if (numPeople > 1) {
-			message = "(Silence - everyone runs to class.)";
-		} else {
-			message = "(Silence - no one is here to say goodbye.)";
-		}
-		numPeople = 0;
-		people.clear();
-		return message;
-	}
+    /**
+     * Clears the table as everyone goes to class.
+     * 
+     * @return a goodbye message
+     */
+    public String classStartingSoon() {
+        String message;
+        if (this.numPeople > 1) {
+            message = "(Silence - everyone runs to class.)";
+        } else {
+            message = "(Silence - no one is here to say goodbye.)";
+        }
+        this.numPeople = 0;
+        this.people.clear();
+        return message;
+    }
 }
--- hw7/SnackTable.java
+++ hw7/SnackTable.java
@@ -6,152 +6,152 @@ import java.util.ArrayList;
  * A table with chairs and people that also has snacks.
  */
 public class SnackTable {
-	/**
-	 * The default number of chairs.
-	 */
-	public static final int NUMBER_OF_CHAIRS = 3;
+    /**
+     * The default number of chairs.
+     */
+    public static final int NUMBER_OF_CHAIRS = 3;
 
-	/**
-	 * The number of snacks in a bag of snacks.
-	 */
-	public static final int BAG_OF_SNACKS = 6;
+    /**
+     * The number of snacks in a bag of snacks.
+     */
+    public static final int BAG_OF_SNACKS = 6;
 
-	private int numChairs;
-	private int numPeople;
-	private ArrayList<String> people;
-	private int numSnacks;
+    private int numChairs;
+    private int numPeople;
+    private ArrayList<String> people;
+    private int numSnacks;
 
-	/**
-	 * Constructs a table with the default number of chairs.
-	 */
-	public SnackTable() {
-		this(SnackTable.NUMBER_OF_CHAIRS);
-	}
+    /**
+     * Constructs a table with the default number of chairs.
+     */
+    public SnackTable() {
+        this(SnackTable.NUMBER_OF_CHAIRS);
+    }
 
-	/**
-	 * Constructs a table with the specified number of chairs.
-	 * 
-	 * @param capacity - the number of seats
-	 * @throws IllegalArgumentException if the capacity is less than 0
-	 */
-	public SnackTable(int capacity) {
-		if (capacity < 0) {
-			throw new IllegalArgumentException("Tables must start with a capacity of 0 or more");
-		}
-		numChairs = capacity;
-		numPeople = 0;
-		people = new ArrayList<String>();
-		numSnacks = 0;
-	}
+    /**
+     * Constructs a table with the specified number of chairs.
+     * 
+     * @param capacity - the number of seats
+     * @throws IllegalArgumentException if the capacity is less than 0
+     */
+    public SnackTable(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Tables must start with a capacity of 0 or more");
+        }
+        this.numChairs = capacity;
+        this.numPeople = 0;
+        this.people = new ArrayList<String>();
+        this.numSnacks = 0;
+    }
 
-	/**
-	 * Returns the number of chairs at this table.
-	 */
-	public int getNumChairs() {
-		return numChairs;
-	}
+    /**
+     * Returns the number of chairs at this table.
+     */
+    public int getNumChairs() {
+        return this.numChairs;
+    }
 
-	/**
-	 * Returns the number of people at this table.
-	 */
-	public int getNumPeople() {
-		return numPeople;
-	}
+    /**
+     * Returns the number of people at this table.
+     */
+    public int getNumPeople() {
+        return this.numPeople;
+    }
 
-	/**
-	 * Returns list of people at this table.
-	 */
-	public String getPeople() {
-		return people.toString();
-	}
+    /**
+     * Returns list of people at this table.
+     */
+    public String getPeople() {
+        return this.people.toString();
+    }
 
-	/**
-	 * Returns the number of snacks at this table.
-	 */
-	public int getNumSnacks() {
-		return numSnacks;
-	}
+    /**
+     * Returns the number of snacks at this table.
+     */
+    public int getNumSnacks() {
+        return this.numSnacks;
+    }
 
-	/**
-	 * Returns true if there are fewer people than seats at this table.
-	 */
-	public boolean emptySeat() {
-		return numPeople < numChairs;
-	}
+    /**
+     * Returns true if there are fewer people than seats at this table.
+     */
+    public boolean emptySeat() {
+        return this.numPeople < numChairs;
+    }
 
-	/**
-	 * Adds a person to this table if there is space.
-	 * 
-	 * @param name - the name of the person to add
-	 * @return a welcome message
-	 */
-	public String addPerson(String name) {
-		if (emptySeat()) {
-			numPeople++;
-			people.add(name);
-			if (numPeople == 1) {
-				return "(Silence - no one is here to say hello.)";
-			}
-			return "Welcome " + name + "!";
-		} else {
-			if (numPeople == 0) {
-				return "(Silence - no one is here to say sorry.)";
-			}
-			return "Sorry - there is no space for you " + name + ".";
-		}
-	}
+    /**
+     * Adds a person to this table if there is space.
+     * 
+     * @param name - the name of the person to add
+     * @return a welcome message
+     */
+    public String addPerson(String name) {
+        if (this.emptySeat()) {
+            this.numPeople++;
+            this.people.add(name);
+            if (this.numPeople == 1) {
+                return "(Silence - no one is here to say hello.)";
+            }
+            return "Welcome " + name + "!";
+        } else {
+            if (this.numPeople == 0) {
+                return "(Silence - no one is here to say sorry.)";
+            }
+            return "Sorry - there is no space for you " + name + ".";
+        }
+    }
 
-	/**
-	 * Removes a person from this table.
-	 * 
-	 * @param name - the name of the person to remove
-	 * @return a goodbye message
-	 */
-	public String removePerson(String name) {
-		boolean wasRemoved = people.remove(name);
-		if (!wasRemoved) {
-			return "Weird! " + name + " was not here!";
-		}
+    /**
+     * Removes a person from this table.
+     * 
+     * @param name - the name of the person to remove
+     * @return a goodbye message
+     */
+    public String removePerson(String name) {
+        boolean wasRemoved = this.people.remove(name);
+        if (!wasRemoved) {
+            return "Weird! " + name + " was not here!";
+        }
 
-		numPeople--;
-		if (numPeople == 0) {
-			return "(Silence - no one is here to say goodbye.)";
-		}
-		return "Bye " + name + ".";
-	}
+        this.numPeople--;
+        if (this.numPeople == 0) {
+            return "(Silence - no one is here to say goodbye.)";
+        }
+        return "Bye " + name + ".";
+    }
 
-	/**
-	 * Adds snacks to this table.
-	 */
-	public void addSnacks() {
-		numSnacks += BAG_OF_SNACKS;
-	}
+    /**
+     * Adds snacks to this table.
+     */
+    public void addSnacks() {
+        this.numSnacks += BAG_OF_SNACKS;
+    }
 
-	/**
-	 * Adds a hungry person to this table if there is space and an available snack.
-	 * A hungry person eats one snack.
-	 * 
-	 * @param name - the name of the person to add
-	 * @return a welcome message
-	 */
-	public String addHungryPerson(String name) {
-		if (emptySeat() && numSnacks > 0) {
-			String welcome = addPerson(name);
-			String eating = name + " says yummy!";
-			numSnacks--;
-			return welcome + " " + eating;
-		}
+    /**
+     * Adds a hungry person to this table if there is space and an available snack.
+     * A hungry person eats one snack.
+     * 
+     * @param name - the name of the person to add
+     * @return a welcome message
+     */
+    public String addHungryPerson(String name) {
+        if (this.emptySeat() && this.numSnacks > 0) {
+            String welcome = this.addPerson(name);
+            String eating = name + " says yummy!";
+            this.numSnacks--;
+            return welcome + " " + eating;
+        }
 
-		if (numPeople == 0) {
-			return "(Silence - no one is here to say sorry.)";
-		}
+        if (this.numPeople == 0) {
+            return "(Silence - no one is here to say sorry.)";
+        }
 
-		// either there were no empty seats or there was no snack
-		if (!emptySeat()) {
-			return "Sorry - there is no space for you " + name + ".";
-		}
+        // either there were no empty seats or there was no snack
+        if (!this.emptySeat()) {
+            return "Sorry - there is no space for you " + name + ".";
+        }
 
-		// no snack
-		return "Sorry - there is no snack for you " + name + ".";
-	}
+        // no snack
+        return "Sorry - there is no snack for you " + name + ".";
+    }
 }
